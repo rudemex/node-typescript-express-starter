34745ebc0f381d6824a75c616924bb1ccdf8587f
Maximiliano Delgado <mdelgado@baufest.com>
2020-12-20 21:56:40 -0300

    chore: fix type in req and res

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
-import express = require('express');
-// Create a new express app instance
-const app: express.Application = express();
-app.get('/', (req, res) => {
+import express, { Request, Response } from 'express';
+
+const app = express();
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 app.listen(3000, () => {
