4a935d63731efdb7269693497c4e7f71b6feca36
Maximiliano Delgado <mdelgado@baufest.com>
2021-01-11 23:23:41 -0300

    chore: add route character api rick

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -1,5 +1,4 @@
 import { Application, Request, Response } from 'express';
-import httpClient from '../utils/http-client';
 
 module.exports = (app: Application, appConfig: any) => {
   const { context } = appConfig['server'];
@@ -7,26 +6,4 @@ module.exports = (app: Application, appConfig: any) => {
   app.get(encodeURI(`${context}/health`), (req: Request, res: Response) => {
     res.status(200).json({ status: 'UP' });
   });
-
-  app.get(
-    encodeURI(`${context}/rick`),
-    async (req: Request, res: Response): Promise<any> => {
-      const params = { name: 'rick', status: 'alive', page: 2 };
-      const { status, data } = await httpClient.prepareResponse(
-        'GET',
-        'https://rickandmortyapi.com/api/character/',
-        {},
-        params,
-      );
-
-      /*const { status, data } = await httpClient.get(
-        'https://rickandmortyapi.com/api/character/',
-        {
-          params,
-        },
-      );*/
-
-      res.status(status).json(data);
-    },
-  );
 };
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,6 +3,7 @@ import { Application, Request, Response } from 'express';
 const appRouter = (app: Application, appConfig: any, version: string) => {
   // Routes
   require('./health')(app, appConfig);
+  require('./character')(app, appConfig);
 
   app.get('/', (req: Request, res: Response) => {
     res.status(200).send(`Welcome to server express - v${encodeURI(version)}`);
