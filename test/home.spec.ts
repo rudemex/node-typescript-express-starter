5f24295fae2be7c96b4464109577703963f6d0e5
Maximiliano Delgado <mdelgado@baufest.com>
2021-01-13 21:51:13 -0300

    chore: add test to root endpoint

---
+++ test/home.spec.ts
@@ -0,0 +1,20 @@
+import request from 'supertest';
+const config = require('config');
+import app from '../src/app';
+const pjson = require('../package.json');
+
+const serverConfig = config['server'];
+
+describe('Endpoint root', () => {
+  it('Should be return status 200', async () => {
+    const result = await request(app).get(`/`).send();
+    expect(result.status).toBe(200);
+  });
+
+  it('Should be return message and version', async () => {
+    const result = await request(app).get(`/`).send();
+    expect(result.text).toEqual(
+      `Welcome to server express - v${pjson['version']}`,
+    );
+  });
+});
