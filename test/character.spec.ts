19672704b47bde6e738a82ec2ede3fff3cfbe6c9
Maximiliano Delgado <mdelgado@baufest.com>
2021-01-17 16:50:59 -0300

    chore: add another it in test

--- test/character.spec.ts
+++ test/character.spec.ts
@@ -6,9 +6,14 @@ const config = require('config');
 const serverConfig = config['server'];
 
 describe('Endpoint Health', () => {
-  it('Should be return status 200', async () => {
+  it('Should be return 200 and characters', async () => {
     const result = await request(app).get(`${serverConfig['context']}/character`).send();
     expect(result.status).toBe(200);
     expect(result.body).not.toBeNull();
+  });
+
+  it('Should dont be return status 200', async () => {
+    const result = await request(app).get(`${serverConfig['context']}/character?name=asdsa`).send();
+    expect(result.status).not.toBe(200);
   });
 });
